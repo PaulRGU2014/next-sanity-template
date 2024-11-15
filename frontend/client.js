dfc65380887bd0a25a1d20ea472fc74efe51659d
Paul Thanataweenont <paul@chronos.email>
2024-11-15 15:35:09 -0700

    update develop dataset

--- frontend/client.js
+++ frontend/client.js
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client';
 
 const client = createClient({
   projectId: 'hvv30l4k',
-  dataset: 'production',
+  dataset: 'develop',
   apiVersion: '2021-03-25', // Update to the latest API version
   useCdn: true, // Enable CDN caching
 });
