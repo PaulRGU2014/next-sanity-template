aa0cfdcaf66dc9af05a6f0a814f1edc48c5d42c1
Paul Thanataweenont <paul@chronos.email>
2025-01-08 00:21:25 -0700

    backup

--- frontend/src/components/TextImageButton/TextImageButton.tsx
+++ frontend/src/components/TextImageButton/TextImageButton.tsx
@@ -2,6 +2,8 @@
 
 import styles from './TextImageButton.module.scss';
 import InViewAnim from './../../utils/InViewAnim/InViewAnim'
+import Image from '@/utils/ImageLoader/ImageLoader';
+import Link from '@/utils/LinkWrapper/LinkWrapper';
 
 interface TextImageButtonProps {
   content: any; // Replace 'any' with the appropriate type
@@ -9,9 +11,20 @@ interface TextImageButtonProps {
 
 export default function TextImageButton({content}: TextImageButtonProps) {
   return(
-    <InViewAnim><div className={styles.component}>
-      <div className={styles.wrapper}>
-        <h5>TextImageButton</h5>
+    <InViewAnim><div className={`${styles.component}`}>
+      <div className={`${styles.wrapper} ${content.mediaPosition === 'right' ? styles.right : ''}`}>
+        <Image
+          className={styles.image}
+          src={content.image.asset._ref}
+          alt={content.image.alt}
+          objectFit="cover"
+          objectPosition="left center"
+        />        
+        <section className={styles.text}>
+          <h3>{content.title}</h3>
+          <p>{content.desc}</p>
+          <Link className={styles.link} href={content.link.url} target={content.link.newTab === false ? "_self" : "_blank"}>{content.link.title}</Link>
+        </section>
       </div>
     </div></InViewAnim>
   );
