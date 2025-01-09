2be14ebaa240b323d3ba716a3213095a3b39155f
Paul Thanataweenont <paul@chronos.email>
2025-01-09 16:05:09 -0700

    update component

--- frontend/src/sanity/schemaTypes/pages.ts
+++ frontend/src/sanity/schemaTypes/pages.ts
@@ -24,6 +24,7 @@ export const pages = defineType({
       type: 'array',
       of: [
         {
+          title: 'Component',
           type: 'reference',
           to: [
           //associateHere
