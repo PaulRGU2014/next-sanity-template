bd83d8d38f8d59d1f26589cb6684fd8f964e7575
Paul Thanataweenont <paul@chronos.email>
2025-01-15 16:11:43 -0700

    Dont track backend

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,6 +1,3 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from '@vercel/analytics/next';
-
 export const metadata = {
   title: 'Ross Leader - Admin',
   description: 'This is the admin interface for Ross Leader.',
@@ -15,8 +12,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={true}>
       <body>
         {children}
-        <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   )
